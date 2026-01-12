36bf35d12ff1dd9ad0ea97b66394a37e0c9394cb
chanzeSE <aurora6w9@gmail.com>
2026-01-12 18:59:44 +0700

    fix home demo l1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
-import Home from './pages/home'
+import Home from './pages/Home'
 import Introduce from './pages/Introduce'
 import NotFound from './components/NotFound'
 
